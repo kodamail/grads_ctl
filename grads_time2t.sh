e6838d78d72507fedbf8fb0bb6c720e35989a7cf
Chihiro Kodama <kodamac@jamstec.go.jp>
2015-08-16 20:05:14 +0900

    change temporary directory

--- grads_time2t.sh
+++ grads_time2t.sh
@@ -1,49 +1,50 @@
 #!/bin/sh
+. common.sh || exit 1
+create_temp
+TEMP_DIR=${BASH_COMMON_TEMP_DIR}
+trap 'finish' 0
+
 export LANG=en
+export PATH=$( cd $( dirname ${BASH_SOURCE:-$0} ); pwd ):${PATH}
 
 CTL=$1
 TIME=$2
 OPT=$3  # -gt
 
 if [ ! -f "${CTL}" ] ; then
-    echo "error in grads_time2t.sh: CTL=${CTL} does not exist."
+    echo "error in grads_time2t.sh: CTL=${CTL} does not exist." >&2
     exit 1
 fi
 if [ "${TIME}" = "" ] ; then
-    echo "error in grads_time2t.sh: TIME is not specified."
+    echo "error in grads_time2t.sh: TIME is not specified." >&2
     exit 1
 fi
 
 # TIME is YYYYMMDD? -> assumed to be YYYYMMDD 00:00:00
 FLAG=$( echo "${TIME}" | grep -e "^[0-9]\{8\}" )
 if [ "${FLAG}" != "" ] ; then
-    TIME=$( date --utc --date "${TIME}" +00z%d%b%Y )
+    TIME=$( date --utc --date "${TIME}" +00z%d%b%Y ) || exit 1
 fi
 
-
-cat > temp_grads_time2t_tmp_$$.gs <<EOF
+#cat > temp_grads_time2t_tmp_$$.gs <<EOF
+cat > ${TEMP_DIR}/temp.gs <<EOF
 'reinit'
 rc = gsfallow('on')
 'xopen ${CTL}'
 t = time2t( ${TIME} )
 time_t = t2time( t )
-ret = write( "temp_grads_time2t_tmp_$$.txt", t )
-ret = write( "temp_grads_time2t_tmp_$$.txt", time_t )
+ret = write( "${TEMP_DIR}/temp.txt", t )
+ret = write( "${TEMP_DIR}/temp.txt", time_t )
 'quit'
 EOF
-grads -blc temp_grads_time2t_tmp_$$.gs > /dev/null
-#T=$( cat temp_grads_time2t_tmp_$$.txt )
-T=$( sed temp_grads_time2t_tmp_$$.txt -e "1,1p" -e d )
-TIME_T=$( sed temp_grads_time2t_tmp_$$.txt -e "2,2p" -e d )
-
-rm temp_grads_time2t_tmp_$$.gs temp_grads_time2t_tmp_$$.txt
-
-#echo ${T}
-#echo ${TIME_T}
+grads -blc ${TEMP_DIR}/temp.gs > /dev/null
+T=$( sed ${TEMP_DIR}/temp.txt -e "1,1p" -e d ) || exit 1
+TIME_T=$( sed ${TEMP_DIR}/temp.txt -e "2,2p" -e d ) || exit 1
+rm ${TEMP_DIR}/temp.gs ${TEMP_DIR}/temp.txt
 
 if [ "${OPT}" != "" ] ; then
-    SEC=$(   date --utc --date "${TIME}"   +%s )
-    SEC_T=$( date --utc --date "${TIME_T}" +%s )
+    SEC=$(   date --utc --date "${TIME}"   +%s ) || exit 1
+    SEC_T=$( date --utc --date "${TIME_T}" +%s ) || exit 1
 
     # TIME of T should be later than TIME, i.e. (TIME:*]
     if [ "${OPT}" = "-gt" -a ! ${SEC_T} -gt ${SEC} ] ; then
@@ -58,10 +59,6 @@ if [ "${OPT}" != "" ] ; then
     elif [ "${OPT}" = "-le" -a ! ${SEC_T} -le ${SEC} ] ; then
 	let T=T-1
     fi
-
 fi
-
-
-
 
 echo ${T}
